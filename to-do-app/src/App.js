01655b87f2b87a3a1d54f552b0f14993bef5e02a
AvdicAdnan <avdic_adnan@hotmail.com>
2022-09-28 19:01:13 +0200

    Removed unnecessary text

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
 import TodoForm from "./components/TodoForm";
-/////////////////////////
+
 function App() {
   return (
     <div className="App">
       <h1 className="title">React To do App</h1>
-      blabla
       <TodoForm />
     </div>
   );
