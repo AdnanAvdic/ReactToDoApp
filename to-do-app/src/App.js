be7d43c8b73e4a4cd49349004a3d85774468708d
AvdicAdnan <avdic_adnan@hotmail.com>
2022-09-28 18:54:09 +0200

    Test

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import TodoForm from "./components/TodoForm";
-
+/////////////////////////
 function App() {
   return (
     <div className="App">
       <h1 className="title">React To do App</h1>
+      blabla
       <TodoForm />
     </div>
   );
